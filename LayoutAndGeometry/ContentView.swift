07f57987f33450481a86e74e0f79756a4c61d385
Ihor Sukhachov <i.sukhachov@gmail.com>
2026-01-31 11:53:46 +0200

    changing alignment guide

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -9,16 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        HStack(alignment: .lastTextBaseline) {
-            Text("Long")
-                .font(Font.caption)
-            Text("Live")
-            
-            Text("The")
-                .font(Font.title)
-            Text("Queen")
-                .font(Font.largeTitle)
+//        HStack(alignment: .lastTextBaseline) {
+//            Text("Long")
+//                .font(Font.caption)
+//            Text("Live")
+//            
+//            Text("The")
+//                .font(Font.title)
+//            Text("Queen")
+//                .font(Font.largeTitle)
+//        }
+        
+        VStack(alignment: .leading) {
+            Text("Hello world")
+                .alignmentGuide(.leading) { d in
+                    d[.trailing]
+                }
+            Text("This is a longer line of text")
         }
+        .background(.red)
+        .frame(width: 400, height: 400)
+        .background(.blue)
     }
 }
 
