f47027068f79159fb98ad6ae6ba2a2cb52eb2a6b
Ihor Sukhachov <i.sukhachov@gmail.com>
2026-01-31 11:25:54 +0200

    Initial Commit

---
+++ LayoutAndGeometry/LayoutAndGeometryApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayoutAndGeometryApp.swift
+//  LayoutAndGeometry
+//
+//  Created by Ihor Sukhachov on 31.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct LayoutAndGeometryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
