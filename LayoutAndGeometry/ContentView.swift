394da8aaf911fe4de3c935a77cfcb7f0225e70bd
Ihor Sukhachov <i.sukhachov@gmail.com>
2026-02-03 13:21:17 +0200

    making things rotate on top and bottom

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -11,18 +11,20 @@ import SwiftUI
 struct ContentView: View {
     let colors: [Color] = [.brown, .blue, .red, .yellow, .green, .gray, .pink, .purple, .orange]
     var body: some View {
+        GeometryReader { fullView in
         ScrollView {
-                   ForEach(0..<50) { index in
-                       GeometryReader { proxy in
-                           Text("Row #\(index)")
-                               .font(.title)
-                               .frame(maxWidth: .infinity)
-                               .background(colors[index % 7])
-                               .rotation3DEffect(.degrees(proxy.frame(in: .global).minY / 5), axis: (x: 0, y: 1, z: 0))
-                       }
-                       .frame(height: 40)
-                   }
-               }
+            ForEach(0..<50) { index in
+                GeometryReader { proxy in
+                    Text("Row #\(index)")
+                        .font(.title)
+                        .frame(maxWidth: .infinity)
+                        .background(colors[index % 7])
+                        .rotation3DEffect(.degrees(proxy.frame(in: .global).minY - fullView.size.height / 2) / 5, axis: (x: 0, y: 1, z: 0))
+                }
+                .frame(height: 40)
+            }
+        }
+    }
     }
 }
 
