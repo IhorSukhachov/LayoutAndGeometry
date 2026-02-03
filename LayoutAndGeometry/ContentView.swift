acbea72ad77f84059c6d3b63b6038e8504abc3da
Ihor Sukhachov <i.sukhachov@gmail.com>
2026-02-03 13:07:08 +0200

    coordinates inside heometry reader

--- LayoutAndGeometry/ContentView.swift
+++ LayoutAndGeometry/ContentView.swift
@@ -7,72 +7,41 @@
 
 import SwiftUI
 
-extension VerticalAlignment {
-    enum MidAccountAndName: AlignmentID {
-        static func defaultValue( in context: ViewDimensions) -> CGFloat {
-            context[.top]
+struct OuterView: View {
+    var body: some View {
+        VStack {
+            Text("Top")
+            InnerView()
+                .background(.green)
+            Text("Bottom")
         }
     }
-    static let midAccountAndName = VerticalAlignment(MidAccountAndName.self)
+}
+
+struct InnerView: View {
+    var body: some View {
+        HStack {
+            Text("Left")
+            GeometryReader { proxy in
+                Text("Center")
+                    .background(.blue)
+                    .onTapGesture {
+                        print("Global center: \(proxy.frame(in: .global).midX) x \(proxy.frame(in: .global).midY)")
+                        print("Custom center: \(proxy.frame(in: .named("Custom")).midX) x \(proxy.frame(in: .named("Custom")).midY)")
+                        print("Local center: \(proxy.frame(in: .local).midX) x \(proxy.frame(in: .local).midY)")
+                    }
+            }
+            .background(.orange)
+            Text("Right")
+        }
+    }
 }
 
 struct ContentView: View {
     var body: some View {
-////        HStack(alignment: .lastTextBaseline) {
-////            Text("Long")
-////                .font(Font.caption)
-////            Text("Live")
-////            
-////            Text("The")
-////                .font(Font.title)
-////            Text("Queen")
-////                .font(Font.largeTitle)
-////        }
-//        
-//        VStack(alignment: .leading) {
-////            Text("Hello world")
-////                .alignmentGuide(.leading) { d in
-////                    d[.trailing]
-////                }
-////            Text("This is a longer line of text")
-//            ForEach(0..<10) {position in
-//                Text("Number \(position)")
-//                    .alignmentGuide(.leading) { _ in
-//                        Double(position) * -10
-//                    }
-//            }
-//        }
-//        .background(.red)
-//        .frame(width: 400, height: 400)
-//        .background(.blue)
-//        HStack(alignment: .midAccountAndName) {
-//            VStack {
-//                Text("Aldrin")
-//                    .alignmentGuide(.midAccountAndName) {d in
-//                        d[VerticalAlignment.center]
-//                    }
-//                Image("aldrin")
-//                    .resizable()
-//                    .frame(width: 64, height: 64)
-//            }
-//            VStack {
-//                Text("Full name:")
-//                
-//                Text("Some Aldrin")
-//                    .alignmentGuide(.midAccountAndName) {d in
-//                        d[VerticalAlignment.center]
-//                    }
-//                    .font(Font.largeTitle)
-//            }
-//        }
-        GeometryReader { proxy in
-            Image("aldrin")
-                .resizable()
-                .scaledToFit()
-                .frame(width: proxy.size.width * 0.8)
-        }
-        
-        
+        OuterView()
+            .background(.red)
+            .coordinateSpace(name: "Custom")
     }
 }
 
